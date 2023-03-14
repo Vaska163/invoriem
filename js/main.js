96da32dcd1612ea31583723aecdb076b0e790cb1
Vasily <xenofontow@gmail.com>
2023-03-14 21:02:05 +0400

    no comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    dots: false,
+    loop: true,
+    items: 3,
+    responsive: {
+      0: {
+        items: 1,
+        margin: 32,
+      },
+
+      601: {
+        items: 1,
+      },
+
+      1001: {
+        items: 3,
+        margin: 48,
+      },
+    },
+  });
 });
+
+/* VIDEO*/
+
+const videoBtn = document.querySelector("#video-btn");
+const videoPicture = document.querySelector(".video__picture");
+const videoWrapper = document.querySelector(".video");
+const video = document.querySelector("#video-file");
+
+videoWrapper.addEventListener("click", function () {
+  if (video.paused) {
+    videoPicture.classList.add("hidden");
+    videoWrapper.classList.remove("video-overlay");
+    videoBtn.classList.add("none");
+    video.play();
+  } else {
+    // videoPicture.classList.remove("none");
+    videoWrapper.classList.add("video-overlay");
+    videoBtn.classList.remove("none");
+    video.pause();
+  }
+});
+
+/* MOBILE NAV */
+
+const openNavbtn = document.querySelector("#openMobileNav");
+const mobileNav = document.querySelector("#mobileNav");
+const closeMobileNav = document.querySelector("#closeMobileNav");
+
+openNavbtn.onclick = function () {
+  mobileNav.classList.remove("none");
+};
+
+closeMobileNav.onclick = function () {
+  mobileNav.classList.add("none");
+};
